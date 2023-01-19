16f3ad9a4362f9e4189baae2ce55d988f1e8350f
KapilKumar2002 <kapil012002@gmail.com>
2023-01-19 14:12:23 +0530

    final commit

--- lib/screens/mainscreens/homescreen.dart
+++ lib/screens/mainscreens/homescreen.dart
@@ -24,44 +24,46 @@ class _HomeScreenState extends State<HomeScreen> {
       key: _globalKey,
       body: SafeArea(
         child: CustomBackground(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                addVerticalSpace(25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                        onTap: () {
-                          _globalKey.currentState!.openDrawer();
-                        },
-                        child: Image.asset("assets/menu.png")),
-                    Text(
-                      "Hi,Anakin",
-                      style: bodyText16w600(color: black),
-                    )
-                  ],
-                ),
-                addVerticalSpace(100),
-                GestureDetector(
-                    onTap: () {
-                      NextScreen(context, PublicNoticeDetailScreen());
-                    },
-                    child: CustomButton(text: "Public Notice", height: 80)),
-                addVerticalSpace(25),
-                GestureDetector(
-                    onTap: () {
-                      NextScreen(context, PaperArticlesScreen());
-                    },
-                    child: CustomButton(text: "Paper Articles", height: 80)),
-                addVerticalSpace(25),
-                GestureDetector(
-                    onTap: () {
-                      NextScreen(context, GRScreen());
-                    },
-                    child: CustomButton(text: "GR", height: 80)),
-              ],
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  addVerticalSpace(25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                          onTap: () {
+                            _globalKey.currentState!.openDrawer();
+                          },
+                          child: Image.asset("assets/menu.png")),
+                      Text(
+                        "Hi,Anakin",
+                        style: bodyText16w600(color: black),
+                      )
+                    ],
+                  ),
+                  addVerticalSpace(100),
+                  GestureDetector(
+                      onTap: () {
+                        NextScreen(context, PublicNoticeDetailScreen());
+                      },
+                      child: CustomButton(text: "Public Notice", height: 80)),
+                  addVerticalSpace(25),
+                  GestureDetector(
+                      onTap: () {
+                        NextScreen(context, PaperArticlesScreen());
+                      },
+                      child: CustomButton(text: "Paper Articles", height: 80)),
+                  addVerticalSpace(25),
+                  GestureDetector(
+                      onTap: () {
+                        NextScreen(context, GRScreen());
+                      },
+                      child: CustomButton(text: "GR", height: 80)),
+                ],
+              ),
             ),
           ),
         ),
